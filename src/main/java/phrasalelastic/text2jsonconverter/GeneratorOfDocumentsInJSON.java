19ea9cccb6a82b17060dad73c03d0222d60b997b
lukaszsienko <l.sienko@stud.elka.pw.edu.pl>
2018-05-24 00:43:20 +0200

    null-safe fix

--- src/main/java/phrasalelastic/text2jsonconverter/GeneratorOfDocumentsInJSON.java
+++ src/main/java/phrasalelastic/text2jsonconverter/GeneratorOfDocumentsInJSON.java
@@ -3,9 +3,11 @@ package phrasalelastic.text2jsonconverter;
 public class GeneratorOfDocumentsInJSON {
 
     public static String generateJSON(String polishText, String englishText) {
+        String plTxt = (polishText == null) ? "" : polishText;
+        String enTxt = (englishText == null) ? "" : englishText;
         return "{\n" +
-                "\r\"cv_pl\" : \""+polishText+"\",\n" +
-                "\r\"cv_en\" : \""+englishText+"\"\n" +
+                "\r\"cv_pl\" : \""+plTxt+"\",\n" +
+                "\r\"cv_en\" : \""+enTxt+"\"\n" +
                 "}";
     }
 

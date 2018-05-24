a185eb6f6f9bc7a2f2b6cfd23fbbf19bcda5efde
lukaszsienko <l.sienko@stud.elka.pw.edu.pl>
2018-05-24 15:59:20 +0200

    added more like this query

---
+++ src/main/java/phrasalelastic/experiments/MoreLikeThis.java
@@ -0,0 +1,61 @@
+package phrasalelastic.experiments;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MoreLikeThisQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MoreLikeThis {
+
+    private RestHighLevelClient client;
+
+    public MoreLikeThis() {
+        client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+    }
+
+    public void doMoreLikeThisSearch(String queryText) {
+        MoreLikeThisQueryBuilder moreLikeThisQueryBuilder =
+                QueryBuilders.moreLikeThisQuery(
+                        new String[] {"cv_pl", "cv_en"},
+                        new String[] {queryText},
+                        new MoreLikeThisQueryBuilder.Item[]{});
+
+
+        SearchRequest request = new SearchRequest("cvbase");
+        request.types("cv");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(moreLikeThisQueryBuilder);
+        request.source(searchSourceBuilder);
+
+
+        try {
+            SearchResponse searchResponse = client.search(request);
+            SearchHits hits = searchResponse.getHits();
+            SearchHit[] searchHits = hits.getHits();
+            int nr = 1;
+            for (SearchHit hit : searchHits) {
+                System.out.println("\rPosition nr "+nr++);
+                Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+                String cv_pl = (String) sourceAsMap.get("cv_pl");
+                System.out.println("CV_PL:");
+                System.out.println(cv_pl);
+                String cv_en = (String) sourceAsMap.get("cv_en");
+                System.out.println("CV_EN:");
+                System.out.println(cv_en);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
